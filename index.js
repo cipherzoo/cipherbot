78d45fa503b136ae03c9dceab1efdde686d3e6a0
tirumudi <tirumudi@gmail.com>
2018-11-26 18:31:03 +0530

    app commit

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 function getInfo(req,res){
 	res.json({
-		text : {
-            text: 'Your competitors are making a progress. '
-		}
+			fulfillmentText:"",
+			fulfillmentMessages:[{
+					text : {
+						text: 'Your competitors are making a progress. '
+			}}]
 	});
 }
 
