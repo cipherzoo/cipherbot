78065137bc8603f697f8ae21b756007ff458255d
tirumudi <tirumudi@gmail.com>
2018-11-26 17:53:31 +0530

    app commit

--- index.js
+++ index.js
@@ -8,29 +8,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-function fallback (agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-}
-
-function getUpdates(agent){
-	return "Your competitor is making a progress. Wanna take a lookup ?"
-}
-
-function WebhookProcessing(req, res) {
-    const agent = new WebhookClient({request: req, response: res});
-    console.info(`agent set`);
-
-    let intentMap = new Map();
-    //intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-	intentMap.set('get-updates', getUpdates);
-    agent.handleRequest(intentMap);
+function getInfo(req,res){
+	res.json({
+            speech: 'Your competitors are making a progress. ',
+            displayText: 'Campaigns to run',
+            source: 'Schedule'
+    });
 }
 
 // Webhook
 app.post('/', function (req, res) {
-    WebhookProcessing(req, res);
+    if(req.body.queryResult.intent.displayName){
+		getInfo(req,res);
+	}
 });
 
 app.listen(process.env.PORT || 8082, function () {
